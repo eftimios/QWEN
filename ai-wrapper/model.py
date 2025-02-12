4b5f59ef580398791162f3fcf8827ae41e3f4b3f
eftimios <saige60vonxzw@outlook.com>
2025-02-12 13:36:23 -0500

    update

--- ai-wrapper/model.py
+++ ai-wrapper/model.py
@@ -137,10 +137,13 @@ class QwenModel:
 
         return retrieved_docs
 
-    def chat_stream(self, query, id, history, use_rag=True):
+    def chat_stream(self, query, id, his, use_rag=True):
         partial_text = ""
-        if history is None or len(history) == 0:
+        
+        if his is []:
             history = self.histories.get(id, [])
+        else:
+            history = his
 
         print(history)
 
